77fcaed09e056860fc2ad4cbf2575e812b8c9328
Jennifer Mead <jennifer.e.mead@gmail.com>
2016-06-20 17:25:32 -0700

    I added more comments to help make the code easier to understand

--- js/app.js
+++ js/app.js
@@ -1,24 +1,28 @@
-//This code allows the star to appear in the bottom left corner
-var Points = function (x, y){
+//This code allows the star to appear in the bottom left-hand corner
+//Contstructor function
+var Star = function (x, y){
   this.sprite = 'images/Star.png';
   this.x = x;
   this.y = y;
 }
 
-Points.prototype.render = function() {
+//this allows the eventual star object to be rendered
+Star.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-var star = new Points(0,505);
+//instantiation of a new star object from the Star constructor function
+var star = new Star(0,505);
 
 
-//This code allows the gems to appear
+//This code allows the gems to appear randomly somewhere on the stone blocks
+//superclass constructor function
 var ExtraPoints = function (){
   this.x = (600*Math.random());
   this.y = (165*Math.random())+50;
-  console.log(this.x);
-}
+  }
 
+//This prevents the two gems from appearing in the same place at once by checking on every update to make sure they are not overlapping. If they are, a new location is chosen for the second gem until they are no longer overlapping
 ExtraPoints.prototype.update = function(){
   if (gem1.x <  gem2.x + 75 &&
     gem1.x + 75 >  gem2.x &&
@@ -30,18 +34,24 @@ ExtraPoints.prototype.update = function(){
 
 };
 
+//the first subclass constructor
+//this creates the blue gem which is worth 1 point
 var BlueGem = function(){
     ExtraPoints.call(this)
     this.sprite = 'images/GemBlue.png';
     this.points = 1;
 }
+
 BlueGem.prototype = Object.create(ExtraPoints.prototype);
 
 BlueGem.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+
 BlueGem.prototype.constructor = BlueGem;
 
+//the second subclass constructor
+//this creates the orange gem with is worth 2 points
 var OrangeGem = function(){
     ExtraPoints.call(this)
     this.sprite = 'images/GemOrange.png';
@@ -53,21 +63,16 @@ OrangeGem.prototype = Object.create(ExtraPoints.prototype);
 OrangeGem.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+
 OrangeGem.prototype.constructor = OrangeGem;
 
-
+//instantiating new gem objects
 var gem1 = new BlueGem();
 var gem2 = new OrangeGem();
 
 
-
-
-
-
-
-
-
-// Enemies our player must avoid. This is an enemy superclass
+// Enemies our player must avoid. They move along the screen horizontally and any collision with them ends the round and causes the player to move back to the beginning
+//This code creates the superclass constructor for the enemy bugs
 var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // The image/sprite for our enemies, this uses
@@ -84,8 +89,8 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-//now I am creating two subclasses, one for enemies that move forward, and one for a single enemy that moves in reverse
-//this is the code for the enemy that moves forward
+//now I am creating two subclass constructors, one for enemies that move forward, and one for a single enemy that moves in reverse
+//this is the sublclass constructor for the enemies that move forward
 var ForwardEnemies = function(){
   Enemy.call(this);
 }
@@ -94,11 +99,9 @@ ForwardEnemies.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    //This sets the speed of movement up until the enemy reaches the end of the canvas. At that point it starts from x = 0 and is given a newly generated random speed
+   At that point it starts from x = 0 and is given a newly generated random speed
     if(this.x < 705){
-        //setting the Enemy speed. I used math random so they weren't all moving at the same speed
-        // Update the enemy's position, required method for game
-        // Parameter: dt, a time delta between ticks
+          //This sets the speed of movement up until the enemy reaches the end of the canvas.
       this.x += (this.speed * dt);
     }
     else{
@@ -108,19 +111,18 @@ ForwardEnemies.prototype.update = function(dt) {
 };
 
 ForwardEnemies.prototype = Object.create(Enemy.prototype);
+
 ForwardEnemies.prototype.constructor = ForwardEnemies;
 
-//code to create the reverse enemy
+//this is the subclass constructor that allows the enemy bug to move in reverse
 var ReverseEnemies = function(){
   Enemy.call(this);
 }
 
 ReverseEnemies.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    //This sets the speed of movement up until the enemy reaches the end of the canvas. At that point it starts from x = 0 and is given a newly generated random speed
+
     if(this.x > 5){
+        //This sets the speed of movement up until the enemy reaches the end of the canvas.
       this.x -= (this.speed * dt);
     }
     else{
@@ -128,20 +130,15 @@ ReverseEnemies.prototype.update = function(dt) {
       this.speed = (250 * Math.random()) + 100;
         }
 };
+
 ReverseEnemies.prototype = Object.create(Enemy.prototype);
+
 ReverseEnemies.prototype.constructor = ReverseEnemies;
 
 
 
-
-
-
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-// Our player
+//this code allows for the creation of the player Object
+//this is the constructor function for the player
 var Player = function(x, y) {
     // The image/sprite for our player, this uses
     // a helper we've provided to easily load images
@@ -154,7 +151,7 @@ var Player = function(x, y) {
 var highScore = 0;
 
 Player.prototype.update = function() {
-      //maybe I only need this for restarting the character position if its hit the water or if its interacted with an enemy?
+      //if the player reaches the water the players position is restarted and a point is gained
       if(this.y <0.5){
         this.y = 400;
         this.score +=1;
@@ -164,6 +161,10 @@ Player.prototype.update = function() {
 
 
 };
+
+//this code checks for collisions between the player and the enemy bugs.
+//if a collision is detected the round ends and the players position is restarted and the score is checked against the high score. If its higher than the high score, the current score
+//becomes the high score
 
 Player.prototype.checkCollisions = function(){
   for(var i=0; i<3; i++){
@@ -184,6 +185,7 @@ Player.prototype.checkCollisions = function(){
   };
 };
 
+//this code checks for collisions between the player and the blue gem. If a collision is detected, the player gains one point and the gem position is reset to a new random position. The player position is not reset.
 Player.prototype.checkBlueCollision = function(){
   if (this.x < gem1.x + 50 &&
    this.x + 50 > gem1.x &&
@@ -195,6 +197,8 @@ Player.prototype.checkBlueCollision = function(){
        }
    };
 
+
+   //this code checks for collisions between the player and the orange gem. If a collision is detected, the player gains two points and the gem position is reset to a new random position. The player position is not reset.
    Player.prototype.checkOrangeCollision = function(){
      if (this.x < gem2.x + 50 &&
       this.x + 50 > gem2.x &&
@@ -206,6 +210,7 @@ Player.prototype.checkBlueCollision = function(){
           }
       };
 
+//this code allows the player to receive input from the keyboard of the computer and tells the player how many pixels to move in any given direction when that key is hit.
 Player.prototype.handleInput = function(key){
 if(key === 'left' && this.x > 0){
 this.x -=100;
@@ -221,7 +226,7 @@ this.y +=85;
 }
 }
 
-// Draw the enemy on the screen, required method for game
+// This code renders the player on the canvas. It also renders the text keeping track of the score onto the canvas.
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     ctx.font = "30px Impact";
@@ -237,15 +242,16 @@ Player.prototype.render = function() {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-allEnemies = new Array();  //should I have Var in front of it?
+//instantiating the forward moving enemy bugs
+var allEnemies = new Array();  //should I have Var in front of it?
 allEnemies[0] = new ForwardEnemies(0, 65);
 allEnemies[1] = new ForwardEnemies(0, 230);
 allEnemies[2] = new ForwardEnemies(0, 310);
 
-
+//instantiating the reverse moving enemy bug
 var reverseEnemies = new ReverseEnemies(700, 145);
 
-
+//instatiating the player
 var player = new Player(200, 400);
 
 
